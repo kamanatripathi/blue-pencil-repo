9a938584cb99ea2af2f4aec2ceac11babd6c8ffb
Kamana Tripathi <kamanatripathi01@gmail.com>
2024-05-26 01:55:37 +0530

    Component Button
    Button component created with 2 variants.

---
+++ src/components/ui/buttons.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { Button as ChakraButton, ButtonProps } from '@chakra-ui/react'
+interface Props extends ButtonProps {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary" ;
+}
+
+const Button = ({ children, variant = "primary", ...props }: Props) => {
+    const baseStyles = {
+        fontFamily: 'Roboto, sans-serif',
+        fontSize: '16px',
+        lineHeight: '26px',
+        borderRadius: '2px',
+        cursor: 'pointer',
+        transition: 'all 0.2s ease-in-out',
+        width: '189px',
+        height: '30px',
+      };
+    
+      const variantStyles = {
+        primary: {
+          ...baseStyles,
+          bg: 'var(--btn-primary-color)',
+          color: 'var(--btn-contrast-color)',
+          border: 'none',
+          _hover: {
+            bg: "var(--btn-contrast-color)",
+            color: "var(--btn-primary-color)",
+            border: '2px solid var(--btn-secondary-color)',
+          },
+        },
+        secondary: {
+          ...baseStyles,
+          bg: 'var(--btn-contrast-color)',
+          color: 'var(--btn-secondary-color)',
+          border: '2px solid var(--btn-secondary-color)',
+          _hover: {
+            bg: "var(--btn-secondary-color)",
+            color:"var(--btn-contrast-color)"
+          },
+        }
+      };
+  return (
+    <ChakraButton {...props} variant={variant} {...variantStyles[variant]}  
+    >
+      {children}
+    </ChakraButton>
+  );
+};
+
+export default Button;
