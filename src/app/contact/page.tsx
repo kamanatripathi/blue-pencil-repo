941dc1e9e5f14bc9a5cb7308811616017c1c542f
Kamana Tripathi <kamanatripathi01@gmail.com>
2024-05-27 00:53:32 +0530

    Contact Page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/ui/footer'
+import Header3 from '@/components/ui/header3'
+import Section8 from '@/components/ui/section-8'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen  flex-col items-center  pt-6">
+    <Header3/>
+    <Section8/>         
+    <Footer/>
+  </main>
+  )
+}
